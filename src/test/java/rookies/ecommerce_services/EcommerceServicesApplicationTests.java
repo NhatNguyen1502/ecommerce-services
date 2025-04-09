d4fb12f78b04a38839ca16c9c1ddd947887bfa3c
Nguyễn Lâm Nhật <zznhatphilong@gmail.com>
2025-04-09 17:51:14 +0700

    init

---
+++ src/test/java/rookies/ecommerce_services/EcommerceServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package rookies.ecommerce_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
